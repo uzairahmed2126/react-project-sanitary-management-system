918daab885a3d8f22f3c8546b5d04de0c4654a60
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-01-21 15:53:29 +0530

    add active links

--- src/components/DefaultLinks/index.jsx
+++ src/components/DefaultLinks/index.jsx
@@ -1,39 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import "../DefaultLinks/index.css";
 function DefaultLinks() {
+  // const navLinkStyle = (isActive) => {
+  //   return {
+  //     fontWeight: isActive ? "bg-red-400" : "",
+  //     textDecoration: isActive ? "none" : "underline",
+  //   };
+  // };
   const path = [
-    ["/", "Home"],
+    ["/", "Log Out", "log-out"],
     ["/accessories", "Accessories"],
     ["/product-order-form", "Product Order Form"],
     ["/view-product", "View Product"],
   ];
   return (
     <div className="grid grid-cols-4 justify-center text-center w-screen font-bold">
-      {path.map(([path, name]) => {
+      {path.map(([path, name, id]) => {
         return (
-          <Link key={path} to={path} className="bg-red-200 p-3 border-2">
+          <NavLink
+            id={id}
+            key={path}
+            to={path}
+            className="bg-red-200 p-3 border-2 hover:bg-red-300"
+          >
             {name}
-          </Link>
+          </NavLink>
         );
       })}
-
-      {/* <Link
-        to="/accessories"
-        className="bg-red-200 p-3 border-2 pointer-events-auto"
-      >
-        Accessories
-      </Link>
-      <Link
-        to="/product-order-form"
-        className="bg-red-200 p-3 border-2 pointer-events-auto"
-      >
-        Product Order Form
-      </Link>
-      <Link
-        to="/view-product"
-        className="bg-red-200 p-3 border-2 pointer-events-auto"
-      >
-        View Product
-      </Link> */}
     </div>
   );
 }
